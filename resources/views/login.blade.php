5f5cee5ceb4496662aceb7b026c98e1f5daed076
syarian09 <ais.share@gmail.com>
2021-01-19 08:23:32 +0800

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>TIK | Login</title>
+	<title>Bimibingan TIK | Login</title>
 	<link href="{{url('/')}}/assets/css/bootstrap.min.css" rel="stylesheet">
 	<link href="{{url('/')}}/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
 	<link href="{{url('/')}}/assets/css/animate.css" rel="stylesheet">
