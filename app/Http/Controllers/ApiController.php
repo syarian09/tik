b2707a1791e15a0ae6ff3c2080231a4e3ae386d5
syarian09 <ais.share@gmail.com>
2021-02-15 08:10:36 +0800

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -19,8 +19,7 @@ class ApiController extends Controller
 
   public function ulangan()
   {
-    $balasan = 'Assalamualaikum ' . $this->sender . ' Silahkan kirim NISN anda sebelum mengerjakan tugas Bimbingan TIK dengan format NISN:,
-    contoh NISN:123456789';
+    $balasan = 'Assalamualaikum ' . $this->sender . ' Silahkan kirim NISN anda sebelum mengerjakan tugas Bimbingan TIK dengan format NISN:, contoh NISN:123456789';
 
     $reply['data'][] = [
       'message' => $balasan,
